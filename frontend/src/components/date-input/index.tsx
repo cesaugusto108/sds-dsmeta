04e9e1668d4c1fc4d7c2255e232ac52c6cb9f6aa
Cesar Augusto <ces.augusto108@gmail.com>
2022-07-13 08:47:35 -0300

    React hook to keep altered date in DatePicker

--- frontend/src/components/date-input/index.tsx
+++ frontend/src/components/date-input/index.tsx
@@ -1,12 +1,32 @@
 import "./styles.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 function DateInput() {
-    return (
+	const [startDate, setStartDate] = useState(new Date(new Date().setDate(new Date().getDate() - 365)));
+	const [endDate, setEndDate] = useState(new Date());
+
+	return (
 		<div className="dsmeta-date-input-container">
-			<DatePicker selected={new Date()} onChange={(date: Date) => {}} className="dsmeta-date-input" dateFormat="dd/MM/yyyy" id="start_date" />
-			<DatePicker selected={new Date()} onChange={(date: Date) => {}} className="dsmeta-date-input" dateFormat="dd/MM/yyyy" id="end_date" />
+			<DatePicker
+				selected={startDate}
+				onChange={(date: Date) => {
+					setStartDate(date);
+				}}
+				className="dsmeta-date-input"
+				dateFormat="dd/MM/yyyy"
+				id="start_date"
+			/>
+			<DatePicker
+				selected={endDate}
+				onChange={(date: Date) => {
+					setEndDate(date);
+				}}
+				className="dsmeta-date-input"
+				dateFormat="dd/MM/yyyy"
+				id="end_date"
+			/>
 		</div>
 	);
 }
