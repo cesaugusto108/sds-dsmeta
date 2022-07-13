9e8e024ad797cb4036ad3f092f5554afae2d5e10
Cesar Augusto <ces.augusto108@gmail.com>
2022-07-12 22:05:46 -0300

    Date input component

--- frontend/src/components/card/index.tsx
+++ frontend/src/components/card/index.tsx
@@ -1,9 +1,11 @@
 import "./styles.css";
+import Date from "../../components/date";
 
 function Card() {
 	return (
 		<div className="dsmeta-card">
 			<h2>Vendas</h2>
+            <Date />
 		</div>
 	);
 }
---
+++ frontend/src/components/date/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+function Date() {
+    return (
+		<div className="dsmeta-date-input-container">
+			<input type="date" name="" id="start_date" className="dsmeta-date-input" />
+			<input type="date" name="" id="end_date" className="dsmeta-date-input" />
+		</div>
+	);
+}
+
+export default Date;
