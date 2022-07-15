737dd53127668b98a6bdc2d9b807eaf27df9eb68
Cesar Augusto <ces.augusto108@gmail.com>
2022-07-15 20:10:53 -0300

    Dynamic content: table rows listing sales

--- frontend/src/components/table-body/index.tsx
+++ frontend/src/components/table-body/index.tsx
@@ -1,19 +1,36 @@
-import TableBodyRow from "../table-body-row";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
+import NotifyButton from "../notify-button";
 import "./styles.css";
 
 function TableBody() {
-	return (
-		<tbody>
-			<TableBodyRow />
-			<TableBodyRow />
-			<TableBodyRow />
-			<TableBodyRow />
-			<TableBodyRow />
-			<TableBodyRow />
-			<TableBodyRow />
-			<TableBodyRow />
-		</tbody>
-	);
+	const [sales, setSales] = useState<Sale[]>([]);
+
+	useEffect(() => {
+		axios.get(`${BASE_URL}/sales`).then((response) => {
+			setSales(response.data.content);
+		});
+	}, []);
+
+	const rows = sales.map((sale) => {
+		return (
+			<tr key={sale.id}>
+				<td className="table-id">{sale.id}</td>
+				<td className="table-date">{new Date(sale.date).toLocaleDateString()}</td>
+				<td className="table-salesperson">{sale.salespersonName}</td>
+				<td className="table-visits">{sale.visits}</td>
+				<td className="table-sales">{sale.deals}</td>
+				<td className="table-total">{sale.total.toFixed(2)}</td>
+				<td className="table-notify">
+					<NotifyButton />
+				</td>
+			</tr>
+		);
+	});
+
+	return <tbody>{rows}</tbody>;
 }
 
 export default TableBody;
---
+++ frontend/src/models/sale.ts
@@ -0,0 +1,8 @@
+export type Sale = {
+    id: number;
+    salespersonName: string;
+    visits: number;
+    deals: number;
+    total: number;
+    date: string;
+}
