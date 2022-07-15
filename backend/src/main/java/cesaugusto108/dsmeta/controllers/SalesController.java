2bc696a8a15b8cd1c3a008bbf7fa999ba278757a
Cesar Augusto <ces.augusto108@gmail.com>
2022-07-14 22:16:46 -0300

    Send SMS using Twilio API

--- backend/src/main/java/cesaugusto108/dsmeta/controllers/SalesController.java
+++ backend/src/main/java/cesaugusto108/dsmeta/controllers/SalesController.java
@@ -2,24 +2,29 @@ package cesaugusto108.dsmeta.controllers;
 
 import cesaugusto108.dsmeta.entities.Sale;
 import cesaugusto108.dsmeta.services.SalesService;
+import cesaugusto108.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SalesController {
     @Autowired
     private SalesService salesService;
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping
     public Page<Sale> findSales(@RequestParam(value = "startDate", defaultValue = "") String startDate,
                                 @RequestParam(value = "endDate", defaultValue = "") String endDate,
                                 Pageable pageable) {
         return salesService.findSales(startDate, endDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void smsNotify(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
