b9e7b9f9c2c4cfcf331237fed9e7df25c6066296
Cesar Augusto <ces.augusto108@gmail.com>
2022-07-16 09:39:23 -0300

    Show notification toast on screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import Card from "./components/card";
 import Header from "./components/header";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 function App() {
 	return (
 		<>
+            <ToastContainer />
 			<Header />
 			<main>
 				<section id="dsmeta-main-section">
